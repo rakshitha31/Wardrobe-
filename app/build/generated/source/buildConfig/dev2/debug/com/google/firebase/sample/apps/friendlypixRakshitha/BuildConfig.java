c7eb466ca31756ba1b8c09fb922abd8d32067040
rakshitha31 <32598162+rakshitha31@users.noreply.github.com>
2017-12-16 21:28:35 +0530

    initial commit

---
+++ app/build/generated/source/buildConfig/dev2/debug/com/google/firebase/sample/apps/friendlypixRakshitha/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.firebase.sample.apps.friendlypixRakshitha;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.firebase.sample.apps.friendlypixRakshitha";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "dev2";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "20160401.02";
+}
